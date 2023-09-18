52d2430e7d1f402d67db0d2fff7b78947c6fa07e
leandro-matheus <mtleandro.oliveira@gmail.com>
2023-09-18 11:04:58 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,24 +2,25 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-    
-            System.out.println("Programa: Por favor, digite o número da Agência:");
-            String agencia = scanner.nextLine();
-    
-            System.out.println("Programa: Por favor, digite o número da Conta:");
-            int numero = scanner.nextInt();
-            scanner.nextLine(); 
-    
-            System.out.println("Programa: Por favor, digite o Nome do Cliente:");
-            String nomeCliente = scanner.nextLine();
-    
-            System.out.println("Programa: Por favor, digite o Saldo da Conta:");
-            double saldo = scanner.nextDouble();
-    
-            scanner.close();
-    
-            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
-        }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Programa: Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Programa: Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer
+
+        System.out.println("Programa: Por favor, digite o Nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Programa: Por favor, digite o Saldo da Conta:");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
+}
+
